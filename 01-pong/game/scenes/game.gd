a564423e734baf3064469feab2624948437d591e
Emerson MX <emersonmx@gmail.com>
2017-09-17 19:02:18 -0300

    Add score

--- 01-pong/game/scenes/game.gd
+++ 01-pong/game/scenes/game.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+var score1 = 0
+var score2 = 0
+
 var ball_class = preload('res://objects/ball.tscn')
 
 func _ready():
@@ -13,8 +16,20 @@ func create_ball():
 	var screen_size = get_viewport_rect().size
 	ball.set_pos(screen_size / 2)
 
+func _update_scores():
+	get_node('scores/score1').set_text(str(score1))
+	get_node('scores/score2').set_text(str(score2))
+
 func _on_out_of_area(body):
 	if !body.is_in_group('ball'):
 		return
+
+	if (body.get_pos() - get_viewport_rect().size / 2).x < 0:
+		score1 += 1
+	else:
+		score2 += 1
+
+	_update_scores()
+
 	body.queue_free()
 	create_ball()
