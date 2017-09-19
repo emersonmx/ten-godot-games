d50a4f769b0baf3864b21eb82d77ade41cf89ed5
Emerson MX <emersonmx@gmail.com>
2017-09-19 01:15:39 -0300

    Move code to snake

--- 02-snake/game/scripts/grid.gd
+++ 02-snake/game/scripts/grid.gd
@@ -17,10 +17,10 @@ func _ready():
 	_create_snake()
 
 func _create_grid():
-	for i in range(grid_size.x):
+	for x in range(grid_size.x):
 		grid.append([])
-		for j in range(grid_size.y):
-			grid[i].append(null)
+		for y in range(grid_size.y):
+			grid[x].append(null)
 
 func _create_snake():
 	var snake = snake_scene.instance()
@@ -28,25 +28,8 @@ func _create_snake():
 	snake.set_pos(map_to_world(grid_size / 2) + half_tile_size)
 	add_child(snake)
 
-func is_cell_vacant(pos, direction):
-	var grid_pos = world_to_map(pos) + direction
-	if grid_pos.x < 0 or grid_pos.x >= grid_size.x:
-		return false
-	if grid_pos.y < 0 or grid_pos.y >= grid_size.y:
-		return false
+func get_cell_content(pos):
+	return grid[pos.x][pos.y]
 
-	return grid[grid_pos.x][grid_pos.y] == null
-
-func get_cell_object(pos, direction):
-	var grid_pos = world_to_map(pos) + direction
-	return grid[grid_pos.x][grid_pos.y]
-
-func update_child_pos(child_node):
-	var grid_pos = world_to_map(child_node.get_pos())
-	grid[grid_pos.x][grid_pos.y] = null
-
-	var new_grid_pos = grid_pos + child_node.direction
-	grid[new_grid_pos.x][new_grid_pos.y] = child_node.type
-
-	var target_pos = map_to_world(new_grid_pos) + half_tile_size
-	return target_pos
+func set_cell_content(pos, type):
+	grid[pos.x][pos.y] = type
--- 02-snake/game/scripts/snake.gd
+++ 02-snake/game/scripts/snake.gd
@@ -5,21 +5,29 @@ const RIGHT = Vector2(1, 0)
 const DOWN = Vector2(0, 1)
 const LEFT = Vector2(-1, 0)
 
-var type
 var direction = Vector2()
+var type
 var target_pos = Vector2()
 
-var _delay = 0.3
+var _delay = 0.2
 var _delay_count = 0
+var _head
+var _tail
+
+var _part_scene = preload('res://objects/snake_part.tscn')
 
 onready var grid = get_parent()
 
 func _ready():
 	type = grid.PLAYER
+	_create_parts()
 	set_fixed_process(true)
 
 func _fixed_process(delta):
 	_move(delta)
+
+func _create_parts():
+	pass
 
 func _move(delta):
 	if Input.is_action_pressed('up'):
@@ -32,8 +40,32 @@ func _move(delta):
 		direction = LEFT
 
 	_delay_count += delta
-	if _delay_count >= _delay:
-		if grid.is_cell_vacant(get_pos(), direction.normalized()):
-			target_pos = grid.update_child_pos(self)
-			set_pos(target_pos)
-			_delay_count = 0
+	if _delay_count < _delay:
+		return
+	_delay_count = 0
+
+	if !_is_inside_of_grid():
+		return
+	if !_can_move():
+		return
+
+	_update_grid_pos()
+
+func _is_inside_of_grid():
+	var grid_pos = grid.world_to_map(get_pos()) + direction
+	if grid_pos.x < 0 or grid_pos.x >= grid.grid_size.x:
+		return false
+	if grid_pos.y < 0 or grid_pos.y >= grid.grid_size.y:
+		return false
+	return true
+
+func _can_move():
+	var pos = grid.world_to_map(get_pos()) + direction
+	return grid.get_cell_content(pos) == null
+
+func _update_grid_pos():
+	var grid_pos = grid.world_to_map(get_pos())
+	var new_grid_pos = grid_pos + direction
+	grid.set_cell_content(grid_pos, null)
+	grid.set_cell_content(new_grid_pos, type)
+	set_pos(grid.map_to_world(new_grid_pos) + grid.half_tile_size)
