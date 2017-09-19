9a95664a5913626d763c993e0cc1cbd6f54b7b10
Emerson MX <emersonmx@gmail.com>
2017-09-19 13:34:56 -0300

    Random food position

--- 02-snake/game/scripts/grid.gd
+++ 02-snake/game/scripts/grid.gd
@@ -10,11 +10,13 @@ var half_tile_size = tile_size / 2
 var grid_size = Vector2(20, 20)
 var grid = []
 
-onready var snake_scene = preload('res://objects/snake.tscn')
+onready var snake_object = preload('res://objects/snake.tscn')
+onready var food_object = preload('res://objects/food.tscn')
 
 func _ready():
 	_create_grid()
 	_create_snake()
+	_create_food()
 
 func _draw_ascii_grid():
 	print('---')
@@ -30,11 +32,27 @@ func _create_grid():
 			grid[x].append(null)
 
 func _create_snake():
-	var snake = snake_scene.instance()
+	var snake = snake_object.instance()
 	add_child(snake)
+
+func _create_food():
+	randomize()
+	var cells = get_empty_cells()
+	var random_cell = cells[randi() % cells.size()]
+	var food = food_object.instance()
+	food.set_pos(map_to_world(random_cell) + half_tile_size)
+	add_child(food)
 
 func get_cell_content(pos):
 	return grid[pos.x][pos.y]
 
 func set_cell_content(pos, type):
 	grid[pos.x][pos.y] = type
+
+func get_empty_cells():
+	var result = []
+	for x in range(grid_size.x):
+		for y in range(grid_size.y):
+			if grid[y][x] == null:
+				result.append(Vector2(x, y))
+	return result
