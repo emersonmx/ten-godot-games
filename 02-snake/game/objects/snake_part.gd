f0e3ccaa9227fe95c015df1c1745d4f2e7e8dab0
Emerson MX <emersonmx@gmail.com>
2017-09-19 02:06:50 -0300

    Add methods to get left and right parts

--- 02-snake/game/objects/snake_part.gd
+++ 02-snake/game/objects/snake_part.gd
@@ -13,3 +13,15 @@ func _ready():
 
 func _fixed_process(delta):
 	pass
+
+func _get_left_part():
+	var left = index - 1
+	if left < 0:
+		return null
+	return snake.parts[left]
+
+func _get_right_part():
+	var right = index + 1
+	if right >= snake.parts.size():
+		return null
+	return snake.parts[right]
