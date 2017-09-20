12f8bcf7cbba5b7d7e87593893be0a0875316d72
Emerson MX <emersonmx@gmail.com>
2017-09-19 21:22:53 -0300

    Add sprites for head, tail and mid body

--- 02-snake/game/objects/snake_part.gd
+++ 02-snake/game/objects/snake_part.gd
@@ -1,7 +1,10 @@
 extends Sprite
 
 enum Direction {
-	UP, RIGHT, DOWN, LEFT
+	UP, RIGHT, DOWN, LEFT, MID, UP_RIGHT, RIGHT_DOWN, DOWN_LEFT, LEFT_UP, BLANK
+}
+
+enum SpriteFrame {
 }
 
 var index
@@ -12,7 +15,59 @@ func _ready():
 	set_fixed_process(true)
 
 func _fixed_process(delta):
-	pass
+	var sprite = BLANK
+	if _is_head():
+		sprite = _get_head_direction()
+	elif _is_tail():
+		sprite = _get_tail_direction()
+	elif _is_mid():
+		sprite = MID
+
+	set_frame(sprite)
+
+func _is_head():
+	return index == 0
+
+func _is_tail():
+	return index == (snake.parts.size() - 1)
+
+func _get_head_direction():
+	var part = _get_right_part()
+	var p1 = get_pos()
+	var p2 = part.get_pos()
+	if p1.x < p2.x:
+		return LEFT
+	if p1.x > p2.x:
+		return RIGHT
+	if p1.y < p2.y:
+		return UP
+	if p1.y > p2.y:
+		return DOWN
+
+func _get_tail_direction():
+	var part = _get_left_part()
+	var p1 = get_pos()
+	var p2 = part.get_pos()
+	if p1.x < p2.x:
+		return LEFT
+	if p1.x > p2.x:
+		return RIGHT
+	if p1.y < p2.y:
+		return UP
+	if p1.y > p2.y:
+		return DOWN
+
+func _is_mid():
+	var left = _get_left_part()
+	var right = _get_right_part()
+	var left_pos = left.get_pos()
+	var right_pos = right.get_pos()
+	var pos = get_pos()
+	if left_pos.x < pos.x < pos.x < right_pos.x:
+		return true
+	if left_pos.x > pos.x > pos.x > right_pos.x:
+		return true
+	return false
 
 func _get_left_part():
 	var left = index - 1
