c10d4d67149d8c3572bd9b7be1be2a662dcc0abc
Emerson MX <emersonmx@gmail.com>
2020-05-06 17:28:08 -0300

    Reset project

--- 03-breakout/game/objects/blocks/block.gd
+++
@@ -1,5 +0,0 @@
-extends StaticBody2D
-
-func _ready():
-	add_to_group('block')
-	add_to_group('update_ball_speed')
--- 06-asteroids/game/objects/explosion/explosion.gd
+++
@@ -1,7 +0,0 @@
-extends AnimatedSprite
-
-func _ready():
-	self.connect('finished', self, '_on_finished')
-
-func _on_finished():
-	queue_free()
