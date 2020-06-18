9d124213d48e9b331fdad416c56fec2c633f36bc
Emerson MX <emersonmx@gmail.com>
2020-06-18 18:51:48 -0300

    Add match point signals

--- pong/scenes/game.gd
+++ pong/scenes/game.gd
@@ -1,5 +1,21 @@
 extends Node2D
 
+signal left_player_score_updated
+signal right_player_score_updated
+
+var left_player_score = 0 setget left_player_score_set
+var right_player_score = 0 setget right_player_score_set
+
+
+func left_player_score_set(value):
+	left_player_score = value
+	emit_signal('left_player_score_updated', left_player_score)
+
+
+func right_player_score_set(value):
+	right_player_score = value
+	emit_signal('right_player_score_updated', right_player_score)
+
 
 func _ready():
 	var play_timer = $play_timer
@@ -35,6 +51,12 @@ func _on_ball_squashed(top_collider, bottom_collider):
 func _on_play_area_body_exited(body):
 	if not body.is_in_group('ball'):
 		return
+
+	var exit_direction = (body.position - position).normalized().x
+	if exit_direction > 0:
+		right_player_score += 1
+	elif exit_direction < 0:
+		left_player_score += 1
 
 	var play_timer = $play_timer
 	play_timer.start()
